b9af247051737406a1f32698b3f5575fc5316581
Aleksey <segeda_aleksey@mai.ru>
2021-12-08 23:18:43 +0300

    init commit

---
+++ .idea/src/main.java
@@ -0,0 +1,59 @@
+public class main {
+
+    public static void main(String[] args) {
+
+        String sity = "Moskow";
+        int population = 10_000_000;
+        int unemployed = 10;
+        char percent = '%';
+        System.out.printf("%s %d %d%c\n", sity, population, unemployed, percent);
+
+        char section = 'b';
+        int place = 25;
+        int cost = 4_000;
+        System.out.printf("%c %d %d\n", section, place, cost);
+
+        String name = "Aleksey";
+        int age = 18;
+        int height = 175;
+        System.out.printf("%s %d %d\n", name, age, height);
+
+        char a = 'a';
+        char b = 'b';
+        char c = 'c';
+        int number = 123;
+        int weight = 1_500;
+        System.out.printf("%c%d%c%c %d\n", a, number, b, c, weight);
+
+        String nameCompani = "Tec";
+        int income = 1_000_000_000;
+        int fraction = 20;
+        System.out.printf("%s %d %d%c\n", nameCompani, income, fraction, percent);
+
+        char rh_factor = '+';
+        int blood_type = 3;
+        int proportion_of_people = 44;
+        System.out.printf("%c %d %d%c\n", rh_factor, blood_type, proportion_of_people, percent);
+
+        String name_of_the_university = "Java university";
+        int student = 1000;
+        int share_of_deducted = 10;
+        System.out.printf("%s %d %d%c\n", name_of_the_university, student, share_of_deducted,percent);
+
+        char rating1 = '*';
+        int copy = 1_000_000_000;
+        int ratingNum = 23;
+        System.out.printf("%c%c%c %d %d\n", rating1, rating1, rating1, copy, ratingNum);
+
+        String brandName = "Java";
+        int namberProduct = 100_000_000;
+        int percentSales = 25;
+        System.out.printf("%s %d %d%c", brandName, namberProduct, percentSales, percent);
+
+        char category = 'f';
+        int barcode = 232435345;
+        int percentage_of_buyers = 50;
+        System.out.printf("%c %d %d%c", category, barcode, percentage_of_buyers, percent);
+
+    }
+}
